b3dfee5cfe9a6c74f6fdaeb1384ac5c9e024ee09
Viktor Karpov <vitka.k@yandex.ru>
2020-08-01 19:05:43 +0300

    add setup py for cxfreeze

---
+++ setup.py
@@ -0,0 +1,11 @@
+import cx_Freeze
+
+exe = [cx_Freeze.Executable("daemon.py", base = "Win32GUI")]
+
+cx_Freeze.setup(
+    name = "GTracker",
+    version = "0.2.2",
+    options = {"build_exe": {"packages": ["psutil", "GTracker", "configparser", "sys"],  
+        "include_files": ["config.ini", "games.txt"]}},
+    executables = exe
+) 
